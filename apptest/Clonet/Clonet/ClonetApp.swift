4940a9099ed54685a1dfbab00ba85458948cc98b
Jimin731 <petitejini@naver.com>
2022-01-10 12:38:41 +0900

    initial commit

---
+++ apptest/Clonet/Clonet/ClonetApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClonetApp.swift
+//  Clonet
+//
+//  Created by Jimin on 2022/01/10.
+//
+
+import SwiftUI
+
+@main
+struct ClonetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
