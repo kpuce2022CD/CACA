d699433214ac114054bd9c0b7966f703b3102911
Jimin731 <petitejini@naver.com>
2022-01-11 20:40:17 +0900

    iOS json parsing

--- apptest/Clonet/Clonet/Test/JsonTest.swift
+++ apptest/Clonet/Clonet/Test/JsonTest.swift
@@ -5,12 +5,67 @@
 //  Created by Hye Min Choi on 2022/01/11.
 //
 
+import UIKit
 import SwiftUI
 
-struct JsonTest: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+var JsonData: [Json] = loadJson("test.json")
+
+struct Json : Decodable, Identifiable {
+    var id : String
+    var subject : String
+    var grade : String
+}
+
+typealias Codable = Decodable & Encodable
+
+class GradeStore : ObservableObject {
+    
+    @Published var grades: [Json]
+    
+    init (grades: [Json] = []) {
+        self.grades = grades
     }
+    
+}
+
+struct JsonTest: View {
+    @ObservedObject var gradeStore: GradeStore = GradeStore(grades: JsonData)
+    var body: some View{
+        List(gradeStore.grades) { grade in
+                    HStack {
+                        Text(grade.subject)
+                            .font(.largeTitle)
+                        Text(grade.grade)
+                            .font(.headline)
+            }
+        }
+    }
+}
+
+func loadJson<T: Decodable>(_ filename: String) -> T {
+    let data: Data
+    
+    // Bundle 에서 파일이름을 통하여 파일의 주소를 가져온다.
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+    else {
+        fatalError("\(filename) not found.")
+    }
+    
+    do {
+        // 위에서 가져온 주소를 활용하여 Data structure 의 객체를 생성한다.
+        // 이는 데이터를 Json decoding 에 활용할 수 있도록 하는 작업이라고 볼 수 있다.
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Could not load \(filename): (error)")
+    }
+    
+    do {
+        // parse the data
+        return try JSONDecoder().decode(T.self, from: data)
+    } catch {
+        fatalError("Unable to parse \(filename): (error)")
+    }
+    
 }
 
 struct JsonTest_Previews: PreviewProvider {
