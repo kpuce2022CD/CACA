fa50dbadc3d90aec1347bec8b3db9a2d8362be90
papav-hub <pavi03@kpu.ac.kr>
2022-01-12 00:32:28 +0900

    add PageViewController to use swift in swiftui

---
+++ apptest/Clonet/Clonet/Test/IamTest.swift
@@ -0,0 +1,22 @@
+//
+//  IamTest.swift
+//  Clonet
+//
+//  Created by Hye Min Choi on 2022/01/11.
+//
+
+import SwiftUI
+
+struct IamTest: View {
+    var body: some View {
+        Text("text")
+    }
+}
+
+struct IamTest_Previews: PreviewProvider {
+    static var previews: some View {
+        IamTest()
+    }
+}
+
+
