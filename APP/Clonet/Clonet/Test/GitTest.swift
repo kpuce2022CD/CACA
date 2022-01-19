97e7e18c732066f171b974d7a3fdc51719fe0f1c
Jimin731 <petitestan@naver.com>
2022-01-19 12:49:21 +0900

    gitTest

--- APP/Clonet/Clonet/Test/GitTest.swift
+++ APP/Clonet/Clonet/Test/GitTest.swift
@@ -11,19 +11,36 @@ import SwiftGit2
 
 let documentURL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
 
+extension Date {
+        func formatDate() -> String {
+                let dateFormatter = DateFormatter()
+            dateFormatter.setLocalizedDateFormatFromTemplate("EEEE, MMM d")
+            return dateFormatter.string(from: self)
+        }
+}
+
 struct GitTest: View {
-
+    
     @State var message = ""
-
-    let localRepoLocation = documentURL.appendingPathComponent("BigMac")
-    let remoteRepoLocation = "https://github.com/light-tech/BigMac.git"
-    let test = "https://git-codecommit.ap-northeast-2.amazonaws.com/v1/repos/test1"
-
+    @State var lastCMuser = ""
+    @State var lastCMmsg = ""
+    @State var lastCMtime = Date()
+//
+//    static let dateformat: DateFormatter = {
+//           let formatter = DateFormatter()
+//            formatter.dateFormat = "YYYY년 M월 d일"
+//            return formatter
+//        }()
+//
+    let localRepoLocation = documentURL.appendingPathComponent("naa")
+    let remoteRepoLocation = "http://3.36.89.105/git-repositories/hye.git"
+    let test = "http://3.36.89.105/git-repositories/hye.git"
+    
     init() {
         // git_libgit2_init()
         Repository.initialize_libgit2()
     }
-
+    
     var body: some View {
         VStack {
             Button("Open test Git repo", action: testGitRepo)
@@ -31,6 +48,9 @@ struct GitTest: View {
             Button("CREATE GIT REPOSITORY", action: createGitRepo)
             ScrollView {
                 Text(message)
+                Text("\n zz")
+                Text(lastCMuser + lastCMmsg)
+                Text(lastCMtime.formatDate())
             }
         }.padding(5)
     }
@@ -45,23 +65,23 @@ struct GitTest: View {
                 .flatMap {
                     repo.commit($0.oid)
                 }
-
+            
             switch latestCommit {
             case let .success(commit):
                 message = "Latest Commit: \(commit.message) by \(commit.author.name)"
-
+                
             case let .failure(error):
                 message = "Could not get commit: \(error)"
             }
-
+            
         case let .failure(error):
             message = "Could not open repository: \(error)"
         }
     }
-
+    
     func cloneGitRepo() {
         let remote: URL = URL(string: remoteRepoLocation)!
-
+        
         let result = Repository.clone(from: remote, to: localRepoLocation)
         switch result {
         case let .success(repo):
@@ -70,20 +90,27 @@ struct GitTest: View {
                 .flatMap {
                     repo.commit($0.oid)
                 }
-
+            
             switch latestCommit {
             case let .success(commit):
-                message = "Latest Commit: \(commit.message) by \(commit.author.name)"
-
+                message = "Latest Commit: \(commit.message) by \(commit.author.name) at \(commit.author.time)"
+                lastCMmsg = commit.message
+                lastCMuser = commit.author.name
+               // lastCMtime = commit.author.time
+                
             case let .failure(error):
                 message = "Could not get commit: \(error)"
             }
-
+            
         case let .failure(error):
             message = "Could not clone repository: \(error)"
         }
     }
-
+    
+    func getBranchList(){
+        
+    }
+    
     func testGitRepo() {
         let result = Repository.at(localRepoLocation)
         switch result {
@@ -93,15 +120,19 @@ struct GitTest: View {
                 .flatMap {
                     repo.commit($0.oid)
                 }
-
+            
             switch latestCommit {
             case let .success(commit):
-                message = "Latest Commit: \(commit.message) by \(commit.author.name)"
-
+                message = "Latest Commit: \(commit.message) by \(commit.author.name) at \(commit.author.time)"
+                
+                lastCMmsg = commit.message
+                lastCMuser = commit.author.name
+                lastCMtime = commit.author.time
+                
             case let .failure(error):
                 message = "Could not get commit: \(error)"
             }
-
+            
         case let .failure(error):
             message = "Could not open repository: \(error)"
         }
