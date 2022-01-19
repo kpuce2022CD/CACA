765fc6373ce167a213db44e2d5e1ccaa6bbfcf4f
paapvhub <pavi03@kpu.ac.kr>
2022-01-19 21:45:45 +0900

    fix create repository

--- APP/socket/create_repository.js
+++ APP/socket/create_repository.js
@@ -1,15 +1,23 @@
 const shell = require('shelljs')
  
 // shell.cd('~')
+var repository_name = "PJY_JJANG"
+var mkdir = "mkdir /var/www/html/git-repositories/" + repository_name + ".git"
+var ip = "3.37.62.93"
 
-var mkdir = "mkdir /var/www/html/git-repositories/0119-1860.git"
-if(shell.exec('ssh -i \"/Users/hyeminchoi/Desktop/CACA/APP/socket/CLONET.pem\" ubuntu@52.79.177.143 \"' + mkdir + '\"').code !== 0) {
+if(shell.exec('ssh -i \"/Users/hyeminchoi/Desktop/CACA/APP/socket/CLONET.pem\" ubuntu@' + ip + ' "' + mkdir + '\"').code !== 0) {
   shell.echo('Error: command failed')
   shell.exit(1)
 }
 
-var bare = "git init --bare /var/www/html/git-repositories/0119-1860.git"
-if(shell.exec('ssh -i \"/Users/hyeminchoi/Desktop/CACA/APP/socket/CLONET.pem\" ubuntu@52.79.177.143 \"' + bare + '\"').code !== 0) {
+var bare = "git init --bare /var/www/html/git-repositories/" + repository_name + ".git"
+if(shell.exec('ssh -i \"/Users/hyeminchoi/Desktop/CACA/APP/socket/CLONET.pem\" ubuntu@' + ip + ' \"' + bare + '\"').code !== 0) {
     shell.echo('Error: command failed')
     shell.exit(1)
   }
+
+var readMe = "touch /var/www/html/git-repositories/" + repository_name + ".git" + "/readME.text"
+if(shell.exec('ssh -i \"/Users/hyeminchoi/Desktop/CACA/APP/socket/CLONET.pem\" ubuntu@' + ip + ' \"' + readMe + '\"').code !== 0) {
+  shell.echo('Error: command failed')
+  shell.exit(1)
+}
