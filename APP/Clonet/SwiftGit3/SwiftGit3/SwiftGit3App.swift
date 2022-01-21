df59853c9fdb4d83c15ab4fe7e75bd3b076c48ae
pjy7814 <pjy7814@naver.com>
2022-01-22 01:03:13 +0900

    test

---
+++ APP/Clonet/SwiftGit3/SwiftGit3/SwiftGit3App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftGit3App.swift
+//  SwiftGit3
+//
+//  Created by 박지영 on 2022/01/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftGit3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
