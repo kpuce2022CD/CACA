6be87d622429a8891107c510601fed2016dd0086
Jiyoung Park <79956705+pjy7814@users.noreply.github.com>
2022-01-23 20:24:12 +0900

    create Repository Server

--- APP/socket/socket_login.js
+++ APP/socket/socket_login.js
@@ -1,4 +1,5 @@
 const { json } = require('express');
+const shell = require('shelljs')
 var express = require('express');
 const { syncBuiltinESMExports } = require('module');
 var app = express();
@@ -51,6 +52,51 @@ io.sockets.on('connection', function(socket){
         connections.splice(connections.indexOf(socket),1);
     
     });
+
+    //사용자 초대하기
+    socket.on('invite', function (data) {
+
+        console.log(data);
+        var jsonInvite = JSON.parse(data);
+        console.log(jsonInvite);
+        // service_invite(email, repo_name)
+
+        connections.splice(connections.indexOf(socket), 1);
+
+    });
+
+    //레포지토리 생성
+    socket.on('createRepo', function (data) {
+
+        console.log(data);
+        var jsonRepo = JSON.parse(data);
+        console.log(jsonRepo);
+
+
+        RepoService(jsonRepo.user_id, jsonRepo.repo_name, jsonRepo.Repo_ec2_ip, jsonRepo.directory_path)
+
+        // ec2에 레포지토리 추가
+
+        var mkdir = "mkdir /var/www/html/git-repositories/" + jsonRepo.repo_name + ".git"
+        if(shell.exec('ssh -i \"/Users/jiyoung/Downloads/CLONET.pem\" ubuntu@' + jsonRepo.Repo_ec2_ip + ' "' + mkdir + '\"').code !== 0) {
+            shell.echo('Error: command failed')
+           
+        } 
+
+        var bare = "git init --bare /var/www/html/git-repositories/" + jsonRepo.repo_name + ".git"
+        if(shell.exec('ssh -i \"/Users/jiyoung/Downloads/CLONET.pem\" ubuntu@' + jsonRepo.Repo_ec2_ip + ' \"' + bare + '\"').code !== 0) {
+            shell.echo('Error: command failed')
+            
+        }
+
+        var readMe = "touch /var/www/html/git-repositories/" + jsonRepo.repo_name + ".git" + "/readME.text"
+        if(shell.exec('ssh -i \"/Users/jiyoung/Downloads/CLONET.pem\" ubuntu@' + jsonRepo.Repo_ec2_ip + ' \"' + readMe + '\"').code !== 0) {
+            shell.echo('Error: command failed')
+        }
+        ////////////
+        connections.splice(connections.indexOf(socket), 1);
+
+    });
 });
 
 
@@ -64,11 +110,11 @@ function loginService(id, pw){
     // 커넥션을 정의합니다.
     // RDS Console 에서 본인이 설정한 값을 입력해주세요.
     var con = mysql.createConnection({
-        host: "-",
-        user: "-",
-        password: "-",
-        database: "clonet_database"
-    });
+      host: "-",
+      user: "admin",
+      password: "-",
+      database: "clonet_database"
+  });
 
     // var login_result = "err";
     var query = "SELECT user_id, user_pw FROM user " + "where user_id = \'" + id + "\' " +  "and user_pw = \'" + pw + "\'" ;
@@ -104,17 +150,17 @@ function loginService(id, pw){
     });
 }
 
-
+//
 function signupService(id, pw, name, email) {
 
     // 커넥션을 정의합니다.
     // RDS Console 에서 본인이 설정한 값을 입력해주세요.
     var con = mysql.createConnection({
-        host: "",
-        user: "",
-        password: "",
-        database: "clonet_database"
-    });
+      host: "-",
+      user: "admin",
+      password: "-",
+      database: "clonet_database"
+  });
 
 
     var query = "INSERT INTO user (user_id, user_pw, user_name, user_email) VALUES (\""+id+"\", \""+pw+"\", \""+name+"\", \""+email+"\")";
@@ -153,6 +199,160 @@ function signupService(id, pw, name, email) {
         }
       });
 }
+
+
+function create_iam_user(iam_name) {
+    var AWS = require('aws-sdk');
+    AWS.config.loadFromPath('./config.json');
+
+    var iam = new AWS.IAM({ apiVersion: '2010-05-08' });
+    var params = {
+        UserName: iam_name
+    };
+    iam.createUser(params, function (err, data) {
+        if (err) console.log(err, err.stack); // an error occurred
+        else console.log(data);           // successful response
+    });
+}
+
+
+//사용자 추가 이메일 발송
+
+function service_invite(email, repo_name){
+  var nodemailer = require('nodemailer');
+  
+  let transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth:{
+          user: 'CACAClonet@gmail.com',
+          pass: 'qwer1234$'
+      }
+  });
+
+  console.log("sendEMail --------- "+email+dataList);
+  
+  var mailOptions = {
+      from: 'CACACLONET@gmail.com',
+      to: email,
+      subject: 'Welcome Clonet',
+      text: email + "\n" + dataList
+      //template: 'index'
+  };
+  
+  transporter.sendMail(mailOptions, function(err, info){
+      if(err){
+          console.log('Error: ',err);
+      }else {
+          console.log('Message send !!!! ' + email);
+      }
+  });    
+};
+
+
+
+
+//레포지토리 생성
+function RepoService(id, repo_name, repo_ec2_ip, directory_path) {
+
+    // 커넥션을 정의합니다.
+    // RDS Console 에서 본인이 설정한 값을 입력해주세요.
+    var con = mysql.createConnection({
+      host: "-",
+      user: "admin",
+      password: "-",
+      database: "clonet_database"
+  });
+
+    var query = "INSERT INTO repository (repo_name, repo_ec2_ip, directory_path) VALUES (\""+repo_name+"\", \""+repo_ec2_ip+"\", \""+directory_path+"\");";
+
+    console.log("Query: " + query);
+
+    con.connect(function(err) {
+        if (err) {
+        //   throw err; // 접속에 실패하면 에러를 throw 합니다.
+            console.log("error connection DB")
+            // return errorMsg;
+        } else {
+          // 접속시 쿼리를 보냅니다.
+            con.query(query, function(err, rows, fields) {
+                console.log("rows : " + String(rows))
+
+                if(String(rows) == "[object Object]"){
+                    var result = Repo_UserService(id, repo_name)
+                    if(result == "COMPLETE"){
+                        console.log("--COMPLETE--")
+                        
+                        
+
+
+                        
+                        io.sockets.emit("createRepo", {repo_RESULT: 'TRUE'});
+                        con.end();
+                        return "COMPLETE"
+                    } else{
+                        return "false"
+                    }
+                    
+                }else{
+                    io.sockets.emit("createRepo", {repo_RESULT: 'FALSE'});
+                    con.end();
+                    console.log("--db-end--")
+                    return "false"
+                }
+
+            });
+        }
+      });
+}
+
+//레포지토리 사용자추가
+function Repo_UserService(id, repo_name) {
+
+    // 커넥션을 정의합니다.
+    // RDS Console 에서 본인이 설정한 값을 입력해주세요.
+    var con = mysql.createConnection({
+      host: "-",
+      user: "admin",
+      password: "-",
+      database: "clonet_database"
+  });
+
+    var query = "INSERT INTO mapping_repo_user (user_id, repo_name) VALUES (\""+id+"\", \""+repo_name+"\");";
+
+    console.log("SignUp Query: " + query);
+
+    con.connect(function(err) {
+        if (err) {
+        //   throw err; // 접속에 실패하면 에러를 throw 합니다.
+            console.log("error connection DB")
+            // return errorMsg;
+        } else {
+          // 접속시 쿼리를 보냅니다.
+            con.query(query, function(err, rows, fields) {
+                console.log("rows : " + String(rows))
+                // console.log("rows : " + rows)
+                // console.log("err : " + err)
+                // console.log("fields : " + fields)
+
+                if(String(rows) == "[object Object]"){
+                    console.log("--COMPLETE--")
+                    io.sockets.emit("repo_map_RESULT", {signup_RESULT: 'TRUE'});
+                    con.end();
+                    return "COMPLETE"
+                }else{
+                    io.sockets.emit("repo_map_RESULT", {signup_RESULT: 'FALSE'});
+                    con.end();
+                    console.log("--db-end--");
+                    return "false"
+                }
+
+            });
+        }
+      });
+}
+
+
+
 
 
 function create_iam_user(iam_name) {
