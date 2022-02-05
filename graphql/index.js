03d5ea88520df7a4d4581858df957c4fc9e1dcc3
Jiyoung Park <79956705+pjy7814@users.noreply.github.com>
2022-02-06 00:06:04 +0900

    Update index.js

--- graphql/index.js
+++ graphql/index.js
@@ -1,4 +1,7 @@
 const { ApolloServer, gql } = require("apollo-server");
+const shell = require('shelljs')
+var express = require('express');
+
 
 const typeDefs = gql`
   type Query {
@@ -9,6 +12,13 @@ const typeDefs = gql`
 const resolvers = {
   Query: {
     ping: () => "pong",
+    // gitLogfile: () => {
+    //     var gitfile = "cd /Users/jiyoung/Library/Developer/CoreSimulator/Devices/FDBC90DB-31E1-4B75-9D2C-9A0A45BD88BE/data/Containers/Data/Application/8B690498-10C7-449C-A658-C800EB1E3BDE/Documents/hey/.git/logs/"
+    //     if(shell.exec(gitfile + ' \"' + 'git log --all --date=format: ' + '%Y-%m-%d %H:%M:%S' + ' --pretty=format:%an,%ad,%s > ./history.csv'+ ' \"').code !== 0) {
+    //         shell.echo('Error: command failed')
+    //     }
+    //     return 'success'
+    // },
   },
 };
 
@@ -17,6 +27,18 @@ const server = new ApolloServer({
     resolvers,
   });
   
-  server.listen().then(({ url }) => {
-    console.log(`Listening at ${url}`);
-  });
+server.listen().then(({ url }) => {
+    // console.log(`Listening at ${url}`);
+
+    console.log("asdfa");
+});
+
+
+
+var location = '/Users/jiyoung/Library/Developer/CoreSimulator/Devices/FDBC90DB-31E1-4B75-9D2C-9A0A45BD88BE/data/Containers/Data/Application/63819302-6BF6-4288-B7D5-BD9CBA6B063F/Documents/hey/'
+var cmd = 'git log --all --date=format:\'%Y-%m-%d %H:%M:%S\' --pretty=format:%an,%ad,%s > ' + location + 'history.csv'
+if(shell.exec(cmd).code !== 0) {
+    shell.echo('Error: command failed')
+}
+
+console.log("아 안되네")
