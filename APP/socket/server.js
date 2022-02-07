db137b2ac40476ce5426a1b6af16d377f81bfdea
Jiyoung Park <79956705+pjy7814@users.noreply.github.com>
2022-02-07 22:14:58 +0900

    Update server.js

--- APP/socket/server.js
+++ APP/socket/server.js
@@ -1,24 +1,56 @@
+const { ApolloServer, gql } = require("apollo-server");
+const shell = require('shelljs')
 var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
+const fs = require('fs')
 
-connections = [];
+const typeDefs = gql`
+  type Query {
+    ping: String
+  }
+`;
 
-server.listen(process.env.PORT || 3000);
-console.log('Server is running ...');
+const resolvers = {
+  Query: {
+    ping: () => "pong",
 
-io.sockets.on('connection', function(socket){
-    connections.push(socket);
-    console.log('Connect: %s sockets are connected', connections.length);
+  },
+};
 
-    socket.on('disconnect', function(data){
-        connections.splice(connections.indexOf(socket),1);
-        console.log('Disconnect: %s sockets are connected', connections.length);
-    });
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });         
+  
+server.listen().then(({ url }) => {
+    // console.log(`Listening at ${url}`);
+  console.log("asdfa");
 
-    socket.on('NodeJS Server Port', function(data){
-        console.log(data);
-        io.sockets.emit('iOS Client Port', {msg: 'Hi iOS Client'});
-    });
+
 });
+
+
+var location = '/Users/jiyoung/Library/Developer/CoreSimulator/Devices/FDBC90DB-31E1-4B75-9D2C-9A0A45BD88BE/data/Containers/Data/Application/E221CCA4-E1B2-446A-B34A-AF7A8E6C43C7/Documents/test/logJSON.json'
+// const jsonData= require('/Users/jiyoung/Library/Developer/CoreSimulator/Devices/FDBC90DB-31E1-4B75-9D2C-9A0A45BD88BE/data/Containers/Data/Application/E221CCA4-E1B2-446A-B34A-AF7A8E6C43C7/Documents/test/logJSON.json'); 
+// console.log(jsonData);
+
+fs.readFile(location, 'utf8' , (err, data) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+
+  function replacer(key, value) {
+    if (typeof value === 'string') {
+      return undefined;
+    }
+    return value;
+  }
+
+  // var jsonString = JSON.stringify()
+  // var useJson = JSON.stringify("[" + data.slice(0, -1) + "]", replacer); 
+
+  // console.log(useJson);
+  const user = JSON.parse("[" + data.slice(0, -1) + "]");
+  console.log(user)
+})
+
