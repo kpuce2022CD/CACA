c7675d95f983ede7ad77410236e4c6fb3f49c677
papavhub <pavi03@kpu.ac.kr>
2022-02-07 23:52:55 +0900

    commit

--- APP/GraphQL/index.js
+++ APP/GraphQL/index.js
@@ -52,14 +52,10 @@ const server = new ApolloServer({
 server.listen().then(({ url }) => {
     // console.log(`Listening at ${url}`);
   console.log("asdfa");
-
-
 });
 
 
 var location = '/Users/jiyoung/Library/Developer/CoreSimulator/Devices/FDBC90DB-31E1-4B75-9D2C-9A0A45BD88BE/data/Containers/Data/Application/E221CCA4-E1B2-446A-B34A-AF7A8E6C43C7/Documents/test/logJSON.json'
-// const jsonData= require('/Users/jiyoung/Library/Developer/CoreSimulator/Devices/FDBC90DB-31E1-4B75-9D2C-9A0A45BD88BE/data/Containers/Data/Application/E221CCA4-E1B2-446A-B34A-AF7A8E6C43C7/Documents/test/logJSON.json'); 
-// console.log(jsonData);
 
 fs.readFile(location, 'utf8' , (err, data) => {
   if (err) {
@@ -73,12 +69,7 @@ fs.readFile(location, 'utf8' , (err, data) => {
     }
     return value;
   }
-
-  // var jsonString = JSON.stringify()
-  // var useJson = JSON.stringify("[" + data.slice(0, -1) + "]", replacer); 
-
-  // console.log(useJson);
-  const user = JSON.parse("[" + data.slice(0, -1) + "]");
-  console.log(user)
+  const log = JSON.parse("[" + data.slice(0, -1) + "]");
+  console.log(log)
 })
 
