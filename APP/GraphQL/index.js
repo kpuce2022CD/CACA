0463fdfe617fdf35ea697299da541175d935ea48
papavhub <pavi03@kpu.ac.kr>
2022-02-07 16:27:20 +0900

    ApolloGraphQL client ing

--- APP/GraphQL/index.js
+++ APP/GraphQL/index.js
@@ -25,13 +25,3 @@ const server = new ApolloServer({
 server.listen().then(({ url }) => {
     console.log(`Listening at ${url}`);
 });
-
-
-
-
-// 로그 만들기 
-// var location = '/Users/jiyoung/Library/Developer/CoreSimulator/Devices/FDBC90DB-31E1-4B75-9D2C-9A0A45BD88BE/data/Containers/Data/Application/63819302-6BF6-4288-B7D5-BD9CBA6B063F/Documents/hey/'
-// var cmd = 'git log --all --date=format:\'%Y-%m-%d %H:%M:%S\' --pretty=format:%an,%ad,%s > ' + location + 'history.csv'
-// if(shell.exec(cmd).code !== 0) {
-//     shell.echo('Error: command failed')
-// }
