1082f3415959e2c8309c4f2250c31a744a3ce751
papavhub <pavi03@kpu.ac.kr>
2022-02-10 11:27:39 +0900

    Log onApper

--- APP/Clonet/Clonet/Repository_MVVM/Repo_View_Log.swift
+++ APP/Clonet/Clonet/Repository_MVVM/Repo_View_Log.swift
@@ -10,29 +10,27 @@ import Foundation
 import Apollo
 
 struct Repo_View_Log: View {
+    
+    @State var repo_n: String
     @StateObject var log_repoViewModel_a = log_repo_ViewModel()
     
+    init(repo_n: String){
+        self.repo_n = repo_n
+    }
+
     var body: some View {
         List(log_repoViewModel_a.Log_repo_list, id: \.id) { log_l in
             Text(log_l.userId + " : " + log_l.commitMsg)
+        }.onAppear(){
+            log_repoViewModel_a.repo_n = self.repo_n
+            log_repoViewModel_a.appear()
         }
-//        VStack{
-//            ForEach(log_repoViewModel_a.Log_repo_list, id: \.id) { s in
-//                VStack{
-//                    Text("commitMsg: \(s.commitMsg)")
-//                    Text("userId: \(s.userId)")
-//                    Text("commitId: \(s.commitId)")
-//                    Text("date: \(s.date)")
-//                    Text("--")
-//                }
-//            }
-//        }
         
     }
 }
 
 struct Repo_View_Log_Previews: PreviewProvider {
     static var previews: some View {
-        Repo_View_Log()
+        Repo_View_Log(repo_n: "TEST")
     }
 }
