8e484918f6b801d7b440b404b5df85d76a5eb3a2
papavhub <pavi03@kpu.ac.kr>
2022-02-12 19:15:01 +0900

    commit

--- WEB/Clonet/About_Customer/src/main/java/caca/about_customer/AboutCustomerApplication.java
+++ WEB/Clonet/About_Customer/src/main/java/caca/about_customer/AboutCustomerApplication.java
@@ -2,12 +2,19 @@ package caca.about_customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AboutCustomerApplication {
+public class AboutCustomerApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AboutCustomerApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return super.configure(builder);
 	}
 
 }
--- WEB/Clonet/Contact_Customer/src/main/java/caca/contact_customer/ContactCustomerApplication.java
+++ WEB/Clonet/Contact_Customer/src/main/java/caca/contact_customer/ContactCustomerApplication.java
@@ -2,12 +2,19 @@ package caca.contact_customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ContactCustomerApplication {
+public class ContactCustomerApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ContactCustomerApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return super.configure(builder);
     }
 
 }
