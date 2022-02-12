64cb134e7ea0efe8ddb39ba66b5ac631181e148b
papavhub <pavi03@kpu.ac.kr>
2022-02-12 21:06:33 +0900

    reSetting

--- WEB/Clonet/Home/src/main/java/caca/home/HomeApplication.java
+++ WEB/Clonet/Home/src/main/java/caca/home/HomeApplication.java
@@ -2,18 +2,12 @@ package caca.home;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HomeApplication extends SpringBootServletInitializer {
+public class HomeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HomeApplication.class, args);
-    }
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return super.configure(builder);
     }
 
 }
--- WEB/Clonet/Login/src/main/java/caca/login/LoginApplication.java
+++ WEB/Clonet/Login/src/main/java/caca/login/LoginApplication.java
@@ -2,18 +2,12 @@ package caca.login;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LoginApplication extends SpringBootServletInitializer {
+public class LoginApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LoginApplication.class, args);
-    }
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return super.configure(builder);
     }
 
 }
