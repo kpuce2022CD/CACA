6dc1f6efd84aed33da5479f3617c429239b62f41
papavhub <pavi03@kpu.ac.kr>
2022-02-12 21:16:09 +0900

    reSetting

--- WEB/Clonet/About_Edit_User/src/main/java/caca/about_edit_user/AboutEditUserApplication.java
+++ WEB/Clonet/About_Edit_User/src/main/java/caca/about_edit_user/AboutEditUserApplication.java
@@ -2,12 +2,18 @@ package caca.about_edit_user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AboutEditUserApplication {
+public class AboutEditUserApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(AboutEditUserApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return super.configure(builder);
+    }
 }
