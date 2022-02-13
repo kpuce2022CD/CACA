4d8ce1f666dedd8c49bc20370df1b04d907443d3
papavhub <pavi03@kpu.ac.kr>
2022-02-14 02:00:12 +0900

    rm .git in React

--- WEB/Back/Home/src/main/java/caca/home/HomeApplication.java
+++ WEB/Back/Home/src/main/java/caca/home/HomeApplication.java
@@ -13,7 +13,6 @@ package caca.home;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
