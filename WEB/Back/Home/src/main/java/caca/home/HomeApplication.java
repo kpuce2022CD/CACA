ac5a91a77a4f7cae3bc50fc72ec7fe855684951e
papavhub <pavi03@kpu.ac.kr>
2022-02-14 01:55:34 +0900

    Connect React & Springboot

--- WEB/Back/Home/src/main/java/caca/home/HomeApplication.java
+++ WEB/Back/Home/src/main/java/caca/home/HomeApplication.java
@@ -1,17 +1,29 @@
-package CACA.Home;
+package caca.home;
+
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class HomeApplication {
+//	@GetMapping("/home")
+//	public String getHome(){
+//		return "Home";
+//	}
+//}
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 @RestController
 public class HomeApplication {
 
-	@RequestMapping("/")
+	@GetMapping("/home")
 	public String home() {
-		return "Hello Docker World";
+		return "Home Front & Back";
 	}
 
 	public static void main(String[] args) {
