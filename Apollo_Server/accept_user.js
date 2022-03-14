1e9be81bd7e2104446a7f3713b1a37d8a8c2690a
papavhub <pavi03@kpu.ac.kr>
2022-03-14 21:47:01 +0900

    Feat: func Email Click to add User

---
+++ Apollo_Server/accept_user.js
@@ -0,0 +1,34 @@
+const knex = require("knex")({
+  client: "mysql",
+  connection: {
+    host: "",
+    user: "admin",
+    password: "",
+    database: "clonet_database",
+  },
+});
+
+var http = require("http");
+var url = require("url");
+
+http.createServer(function (req, res) {
+    var uri = req.url;
+    var query = url.parse(uri, true).query;
+
+    if (req.method == "GET") {
+      res.writeHead(200, { "Content-type": "text/html" });
+      res.end("ID: " + query.user_id + "repo_name: " + query.repo_name);
+
+      if(query.user_id != "" && query.repo_name != ""){
+        knex('mapping_repo_user').insert({ user_id: query.user_id, repo_name: query.repo_name })
+            .then(function (result) {
+            console.log(result)
+        });
+      }
+
+      
+    }
+  })
+  .listen(8080, function () {
+    console.log("server running on 8080.");
+  });
