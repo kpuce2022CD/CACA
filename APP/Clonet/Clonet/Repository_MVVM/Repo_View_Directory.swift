db68055a393cf48d5440e57666e7ff44053eef53
papavhub <pavi03@kpu.ac.kr>
2022-03-15 14:28:01 +0900

    Feat: Fixel Change Func

--- APP/Clonet/Clonet/Repository_MVVM/Repo_View_Directory.swift
+++ APP/Clonet/Clonet/Repository_MVVM/Repo_View_Directory.swift
@@ -303,11 +303,13 @@ struct Repo_View_Directory: View {
                             List{
                                 ForEach(Repo_ViewModel_req.Req_repo_list, id: \.id) { s in
                                     Button(s.request_context, action: {
-                                        print(s.request_context)
+                                        print("processPixels")
                                     })
                                     
                                 }
                             }.frame(width: 300)
+                            
+                            
                             Button {
                                 presentingToast = false
                             }label: {
@@ -340,7 +342,12 @@ struct Repo_View_Directory: View {
                                 
                             } else {
                                 GeometryReader { geometryProxy in
-                                    ZStack {
+                                    VStack {
+                                        
+                                        // MARK: Fixel Diff
+                                        Image(uiImage: processPixels(in: load(fileName: fileNameImg)!) ?? load(fileName: fileNameImg)!)
+                                            .frame(width: 10, height: 10)
+                                        
                                         Image(uiImage: load(fileName: fileNameImg)!)
                                             .resizable()
                                             .aspectRatio(contentMode: .fit)
@@ -353,6 +360,7 @@ struct Repo_View_Directory: View {
                                                 default:
                                                     break
                                                 }
+                                                
                                             })
                                             .toast(isPresented: $messageToast) {
                                                 ToastView {
@@ -463,5 +471,93 @@ struct Repo_View_Directory: View {
 struct Repo_View_Directory_Previews: PreviewProvider {
     static var previews: some View {
         Repo_View_Directory(repo_n: "TEST", ec2_id: "3.34.194.172", user_id: "user_id")
+    }
+}
+
+
+// MARK: Pixel Change
+func processPixels(in image: UIImage) -> UIImage? {
+    guard let inputCGImage = image.cgImage else {
+        print("unable to get cgImage")
+        return nil
+    }
+    let colorSpace       = CGColorSpaceCreateDeviceRGB()
+    let width            = inputCGImage.width
+    let height           = inputCGImage.height
+    let bytesPerPixel    = 4
+    let bitsPerComponent = 8
+    let bytesPerRow      = bytesPerPixel * width
+    let bitmapInfo       = RGBA32.bitmapInfo
+
+    guard let context = CGContext(data: nil, width: width, height: height, bitsPerComponent: bitsPerComponent, bytesPerRow: bytesPerRow, space: colorSpace, bitmapInfo: bitmapInfo) else {
+        print("unable to create context")
+        return nil
+    }
+    context.draw(inputCGImage, in: CGRect(x: 0, y: 0, width: width, height: height))
+
+    guard let buffer = context.data else {
+        print("unable to get context data")
+        return nil
+    }
+
+    let pixelBuffer = buffer.bindMemory(to: RGBA32.self, capacity: width * height)
+
+    for row in 0 ..< Int(height) {
+        for column in 0 ..< Int(width) {
+            let offset = row * width + column
+            if pixelBuffer[offset] == .black {
+                pixelBuffer[offset] = .red
+            }else{
+                pixelBuffer[offset] = .green
+            }
+        }
+    }
+
+    let outputCGImage = context.makeImage()!
+    let outputImage = UIImage(cgImage: outputCGImage, scale: image.scale, orientation: image.imageOrientation)
+
+    return outputImage
+}
+
+struct RGBA32: Equatable {
+    private var color: UInt32
+
+    var redComponent: UInt8 {
+        return UInt8((color >> 24) & 255)
+    }
+
+    var greenComponent: UInt8 {
+        return UInt8((color >> 16) & 255)
+    }
+
+    var blueComponent: UInt8 {
+        return UInt8((color >> 8) & 255)
+    }
+
+    var alphaComponent: UInt8 {
+        return UInt8((color >> 0) & 255)
+    }
+
+    init(red: UInt8, green: UInt8, blue: UInt8, alpha: UInt8) {
+        let red   = UInt32(red)
+        let green = UInt32(green)
+        let blue  = UInt32(blue)
+        let alpha = UInt32(alpha)
+        color = (red << 24) | (green << 16) | (blue << 8) | (alpha << 0)
+    }
+
+    static let red     = RGBA32(red: 255, green: 0,   blue: 0,   alpha: 255)
+    static let green   = RGBA32(red: 0,   green: 255, blue: 0,   alpha: 255)
+    static let blue    = RGBA32(red: 0,   green: 0,   blue: 255, alpha: 255)
+    static let white   = RGBA32(red: 255, green: 255, blue: 255, alpha: 255)
+    static let black   = RGBA32(red: 0,   green: 0,   blue: 0,   alpha: 255)
+    static let magenta = RGBA32(red: 255, green: 0,   blue: 255, alpha: 255)
+    static let yellow  = RGBA32(red: 255, green: 255, blue: 0,   alpha: 255)
+    static let cyan    = RGBA32(red: 0,   green: 255, blue: 255, alpha: 255)
+
+    static let bitmapInfo = CGImageAlphaInfo.premultipliedLast.rawValue | CGBitmapInfo.byteOrder32Little.rawValue
+
+    static func ==(lhs: RGBA32, rhs: RGBA32) -> Bool {
+        return lhs.color == rhs.color
     }
 }
