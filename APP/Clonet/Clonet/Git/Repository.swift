ad560020571361c1e4186338cea6d5f59ee1288f
papavhub <pavi03@kpu.ac.kr>
2022-03-16 18:09:57 +0900

    Feat: Push Branch

--- APP/Clonet/Clonet/Git/Repository.swift
+++ APP/Clonet/Clonet/Git/Repository.swift
@@ -208,9 +208,7 @@ extension Repository {
     
     // MARK: PUSH
     public func push(_ repo: Repository, _ username: String, _ password: String, _ branch: String? = nil){
-        // todo get this properly
-        
-        let credentials: Credentials = Credentials.plaintext(username: username, password: password)
+        let credentials: Credentials = Credentials.plaintext(username: "ubuntu", password: "qwer1234")
         var options = pushOptions(credentials: credentials)
         
         let repository: OpaquePointer = repo.pointer
@@ -225,43 +223,7 @@ extension Repository {
                 }
         }
         
-        var master: String = ""
-        if(branch == nil){
-            if case .success = reference(named: "refs/heads/master") {
-                master = "refs/heads/master"
-                print("master-main")
-            } else {
-                let branchResult = repo.localBranches()
-                switch branchResult {
-                case .success(let branches):
-                    print("found repo to use: \(branches[0].longName)") //get the first one for now :)
-                    master = branches[0].longName
-                    break
-                case .failure:
-                    print("Failed to get any branches...")
-                    break
-                }
-            }
-        } else {
-            if case .success = reference(named: branch!) {
-                master = "\(branch!)"
-                print("master")
-            } else {
-                // Branch not found.
-                var gitBranch: OpaquePointer?
-                
-                git_branch_create(&gitBranch, repository, branch!, nil, 1);
-                print("Branch not found")
-                master = "\(branch!)"
-            }
-        }
-        
-        if(master == ""){
-            master = "refs/heads/master" // Prevents a crash below
-        }
-        
-        
-        print(master)
+        var master = "refs/heads/" + branch!
         
         let strings: UnsafeMutablePointer<UnsafeMutablePointer<CChar>?> = [master].withUnsafeBufferPointer {
             let buffer = UnsafeMutablePointer<UnsafeMutablePointer<CChar>?>.allocate(capacity: $0.count + 1)
@@ -278,6 +240,75 @@ extension Repository {
         let push_result = git_remote_push(remote, &gitstr, &options)
         print(push_result)
         git_remote_free(remote)
+        
+//        let credentials: Credentials = Credentials.plaintext(username: username, password: password)
+//        var options = pushOptions(credentials: credentials)
+//
+//        let repository: OpaquePointer = repo.pointer
+//        var remote: OpaquePointer? = nil
+//        let result_git_remote_lookup = git_remote_lookup(&remote, repository, "origin" )
+//
+//
+//        func localBranches() -> Result<[Branch], NSError> {
+//            return references(withPrefix: "refs/heads/")
+//                .map { (refs: [ReferenceType]) in
+//                    return refs.map { $0 as! Branch }
+//                }
+//        }
+//
+//        var master: String = ""
+//        if(branch == nil){
+//            if case .success = reference(named: "refs/heads/master") {
+//                master = "refs/heads/master"
+//                print("master-main")
+//            } else {
+//                let branchResult = repo.localBranches()
+//                switch branchResult {
+//                case .success(let branches):
+//                    print("found repo to use: \(branches[0].longName)") //get the first one for now :)
+//                    master = branches[0].longName
+//                    break
+//                case .failure:
+//                    print("Failed to get any branches...")
+//                    break
+//                }
+//            }
+//        } else {
+//            if case .success = reference(named: branch!) {
+//                master = "\(branch!)"
+//                print("master")
+//            } else {
+//                // Branch not found.
+//                var gitBranch: OpaquePointer?
+//
+//                git_branch_create(&gitBranch, repository, branch!, nil, 1);
+//                print("Branch not found")
+//                master = "\(branch!)"
+//            }
+//        }
+//
+//        if(master == ""){
+//            master = "refs/heads/master" // Prevents a crash below
+//        }
+//
+//
+//        print(master)
+//
+//        let strings: UnsafeMutablePointer<UnsafeMutablePointer<CChar>?> = [master].withUnsafeBufferPointer {
+//            let buffer = UnsafeMutablePointer<UnsafeMutablePointer<CChar>?>.allocate(capacity: $0.count + 1)
+//            let val = $0.map
+//            { $0.withCString(strdup) }
+//            buffer.initialize(from: val, count: 1)
+//            buffer[$0.count] = nil
+//            return buffer
+//        }
+//        var gitstr = git_strarray()
+//        gitstr.strings = strings
+//        gitstr.count = 1
+//
+//        let push_result = git_remote_push(remote, &gitstr, &options)
+//        print(push_result)
+//        git_remote_free(remote)
     }
     
     
