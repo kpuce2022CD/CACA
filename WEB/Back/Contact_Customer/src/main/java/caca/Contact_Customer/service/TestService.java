97fc1c364e457a3c2c49b5cc969923fa61f4d15c
pjy7814 <pjy7814@naver.com>
2022-04-13 15:51:13 +0900

    Remove: Web delete web files

--- WEB/Back/Contact_Customer/src/main/java/caca/Contact_Customer/service/TestService.java
+++
@@ -1,9 +0,0 @@
-package caca.Contact_Customer.service;
-
-import caca.Contact_Customer.dto.TestDto;
-
-import java.util.List;
-
-public interface TestService {
-    public List<TestDto> getUserList();
-}
--- WEB/Front/contact_edit_user/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-npm install express
-
-npm run build
