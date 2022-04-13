01c76030021dfa84040898634bdb9a5967008303
Jimin731 <petitestan@naver.com>
2022-04-14 00:33:31 +0900

    Feat: about_customer DB

--- WEB/Back/About_Customer/src/main/java/caca/About_Customer/controller/TestController.java
+++ WEB/Back/About_Customer/src/main/java/caca/About_Customer/controller/TestController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:8001")
 @RequestMapping("/api")
 public class TestController {
     @Autowired
--- WEB/Front/about_customer/src/about_cst_test.jsx
+++ WEB/Front/about_customer/src/about_cst_test.jsx
@@ -105,75 +105,12 @@ class about_cst_test extends Component {
                     </tbody>
                     </table>
                     </div>
-                    
-
-                    {/* <p>Duis aute irure dolor in reprehenderit in voluptate velit esse
-                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-                    <h3>Incididunt ut labore </h3>
-
-                    <p>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
-                        Phasellus
-                        iaculis magna sagittis elit sagittis, at hendrerit lorem venenatis. Morbi accumsan iaculis
-                        blandit.
-                        Cras ultrices hendrerit nisl.</p>
-
-                    <h3>Ut enim ad minim veniam </h3>
-
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas luctus at sem quis varius.
-                        Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
-                        Phasellus iaculis magna sagittis elit sagittis, at hendrerit lorem venenatis. Morbi accumsan
-                        iaculis
-                        blandit. Cras ultrices hendrerit nisl.</p>
-                    <h3>Phasellus iaculis magna </h3>
-                    <p>Duis aute irure dolor in reprehenderit in voluptate velit esse
-                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> */}
-
                     <a href="/contact_customer" className="btn btn-primary" title=""> Get in touch</a>
                 </div>
             </div>
         </main>
 </div>
 );
-        // render(){
-        //     return (
-        //         <div>
-        //             <h2 className="text-center">Boards List</h2>
-        //             <div className ="row">
-        //                 <table className="table table-striped table-bordered">
-        //                     <thead>
-        //                         <tr>
-        //                             <th>글 번호</th>
-        //                             <th>타이틀 </th>
-        //                             <th>작성자 </th>
-        //                             <th>작성일 </th>
-        //                             <th>갱신일 </th>
-        //                             <th>좋아요수</th>
-        //                             {/* <th>조회수</th> */}
-        //                         </tr>
-        //                     </thead>
-        //                     <tbody>
-        //                         {
-        //                             this.state.userAbout.map(
-        //                                 board => 
-        //                                 <tr key = {board.user_id}>
-        //                                     <td> {board.user_id} </td>
-        //                                     <td> {board.user_pw} </td>
-        //                                     <td> {board.user_email} </td>
-        //                                     <td> {board.profilePic} </td>
-        //                                     <td> {board.about} </td>
-        //                                     <td> {board.contact} </td>
-        //                                     {/* <td> {board.counts} </td> */}
-        //                                 </tr>
-        //                             )
-        //                         }
-        //                     </tbody>
-        //                 </table>
-        //             </div>
-        //         </div>
-        //     );
         }
 };
 
