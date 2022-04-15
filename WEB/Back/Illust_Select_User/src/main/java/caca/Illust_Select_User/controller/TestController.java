4ff969eea4177ac1011e3b7da303206b0c2dc2c3
papavhub <pavi03@kpu.ac.kr>
2022-04-15 21:32:15 +0900

    Feat: Illust_Select_User Back COMPLETE

--- WEB/Back/Illust_Select_User/src/main/java/caca/Illust_Select_User/controller/TestController.java
+++ WEB/Back/Illust_Select_User/src/main/java/caca/Illust_Select_User/controller/TestController.java
@@ -8,22 +8,17 @@ import caca.Illust_Select_User.service.TestService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-import java.util.Collections;
+import java.io.ByteArrayOutputStream;
 import java.util.List;
-import java.util.Properties;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.Consumer;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
 
 
 @RestController
@@ -31,10 +26,6 @@ import org.apache.kafka.clients.consumer.KafkaConsumer;
 @CrossOrigin("*")
 public class TestController {
     private final TestService testService;
-    private static final String LOGIN_MEMBER = "";
-    private TestService user_id;
-    private HttpSession session;
-
     private final KafkaProducer kafkaProducer;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
@@ -48,8 +39,8 @@ public class TestController {
     }
 
     @RequestMapping(value = "/piece_u", method = {RequestMethod.GET, RequestMethod.POST})
-    public String Repo_param(HttpServletRequest req) {
-        String Final_Result = "";
+    public String Repo_param(HttpServletRequest req) throws Exception {
+        String Final_result = "";
         
         String user_id = req.getParameter("user_id");
         List<RepoDto> RepoList = testService.getUserRepoList(user_id); // user_id의 RepoList DB에서 받아오기
@@ -58,30 +49,66 @@ public class TestController {
             kafkaProducer.sendPieceNameMessage(RepoList.get(i).getRepo_name());
         }
 
-        Properties props = new Properties();
-        props.put("bootstrap.servers", "172.18.0.44:9092");
-        props.put("group.id", "foo");
-        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        // EC2 get Repository List
+        String host="13.209.116.111";
+	    String username="ubuntu";
+	    String password="qweR1234@";
+        int port = 22;
 
+        Thread.sleep(10000);
 
-        KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(props);
-        kafkaConsumer.subscribe(Collections.singletonList("piece_u"));
+        for (int i = 0; i < RepoList.size(); i++){
+            String cmd_ls = String.format("ls /var/www/html/clonet-repo/%s/", RepoList.get(i).getRepo_name());
+            String result = listFolderStructure(username, password, host, port, cmd_ls);
+            String[] Result = result.split("\n");
 
-        try {
-            while (true) {
-                ConsumerRecords<String, String> records = kafkaConsumer.poll(100);
-                for (ConsumerRecord<String, String> record : records){
-                    System.out.println(record.value());
-                    Final_Result += record.value().toString();
-                }
+            for (int j=0 ; j < Result.length; j++){
+                Final_result += String.format("%s/%s,", RepoList.get(i).getRepo_name(), Result[j]);
             }
-        } finally {
-            kafkaConsumer.close();
-            System.out.println("message: " + Final_Result);
-            return Final_Result;
         }
+            
+        System.out.println("Final message : " + Final_result);
+
+        return Final_result;
+
     }
+
+    public static String listFolderStructure(String username, String password, String host, int port, String command) throws Exception {
+      
+        Session session = null;
+        ChannelExec channel = null;
+        
+        try {
+            session = new JSch().getSession(username, host, port);
+            session.setPassword(password);
+            session.setConfig("StrictHostKeyChecking", "no");
+            session.connect();
+            
+            channel = (ChannelExec) session.openChannel("exec");
+            channel.setCommand(command);
+            ByteArrayOutputStream responseStream = new ByteArrayOutputStream();
+            channel.setOutputStream(responseStream);
+            channel.connect();
+            
+            while (channel.isConnected()) {
+                Thread.sleep(100);
+            }
+            
+            String responseString = new String(responseStream.toByteArray());
+          //   System.out.println(responseString);
+  
+            return responseString;
+  
+  
+          } finally {
+              if (session != null) {
+                  session.disconnect();
+              }
+              if (channel != null) {
+                  channel.disconnect();
+              }
+          }
+      }
 
     // @PostMapping("/piece")
 	// public String sendPieceName(@RequestParam("piece") String arr){
