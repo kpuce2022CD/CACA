5c875d6197fb318faec52993f42a5bb2c04a8016
papavhub <pavi03@kpu.ac.kr>
2022-05-30 17:52:35 +0900

    Feat: currentBranchName variable

--- APP/Clonet/Clonet/Git/Repository.swift
+++ APP/Clonet/Clonet/Git/Repository.swift
@@ -65,14 +65,6 @@ extension Repository {
         var git_m = git_merge(repo.pointer, &git_annotated_commit_from_fetchhead_out, 1, nil, nil)
         print("merge : ", git_m)
         
-//        var git_s = git_signature()
-//        var git_ss = git_signature_now(&git_s, "name", "email")
-//
-//        var git_c_c = git_commit_create(&revert_git_oid, repo.pointer, <#T##update_ref: UnsafePointer<CChar>!##UnsafePointer<CChar>!#>, gitAuthor, gitAuthor, "merge Success1", "merge Success2", <#T##tree: OpaquePointer!##OpaquePointer!#>, <#T##parent_count: Int##Int#>, <#T##parents: UnsafeMutablePointer<OpaquePointer?>!##UnsafeMutablePointer<OpaquePointer?>!#>)
-        
-
-        
-        
     }
     
     // MARK: merge for Repo_View_Git
@@ -207,7 +199,8 @@ extension Repository {
     
     
     // MARK: PUSH
-    public func push(_ repo: Repository, _ username: String, _ password: String, _ branch: String? = nil){
+    public func push(_ repo: Repository, _ username: String, _ password: String, _ branch: String){
+        print("Repository Push ", branch)
         let credentials: Credentials = Credentials.plaintext(username: "ubuntu", password: "qwer1234")
         var options = pushOptions(credentials: credentials)
         
@@ -223,7 +216,9 @@ extension Repository {
                 }
         }
         
-        var master = "refs/heads/" + branch!
+        var branchName = branch.components(separatedBy: "/")
+        var master = "+refs/heads/\(branchName[1]):refs/heads/\(branchName[1])"
+        print("Repository Push ", master)
         
         let strings: UnsafeMutablePointer<UnsafeMutablePointer<CChar>?> = [master].withUnsafeBufferPointer {
             let buffer = UnsafeMutablePointer<UnsafeMutablePointer<CChar>?>.allocate(capacity: $0.count + 1)
@@ -241,74 +236,6 @@ extension Repository {
         print(push_result)
         git_remote_free(remote)
         
-//        let credentials: Credentials = Credentials.plaintext(username: username, password: password)
-//        var options = pushOptions(credentials: credentials)
-//
-//        let repository: OpaquePointer = repo.pointer
-//        var remote: OpaquePointer? = nil
-//        let result_git_remote_lookup = git_remote_lookup(&remote, repository, "origin" )
-//
-//
-//        func localBranches() -> Result<[Branch], NSError> {
-//            return references(withPrefix: "refs/heads/")
-//                .map { (refs: [ReferenceType]) in
-//                    return refs.map { $0 as! Branch }
-//                }
-//        }
-//
-//        var master: String = ""
-//        if(branch == nil){
-//            if case .success = reference(named: "refs/heads/master") {
-//                master = "refs/heads/master"
-//                print("master-main")
-//            } else {
-//                let branchResult = repo.localBranches()
-//                switch branchResult {
-//                case .success(let branches):
-//                    print("found repo to use: \(branches[0].longName)") //get the first one for now :)
-//                    master = branches[0].longName
-//                    break
-//                case .failure:
-//                    print("Failed to get any branches...")
-//                    break
-//                }
-//            }
-//        } else {
-//            if case .success = reference(named: branch!) {
-//                master = "\(branch!)"
-//                print("master")
-//            } else {
-//                // Branch not found.
-//                var gitBranch: OpaquePointer?
-//
-//                git_branch_create(&gitBranch, repository, branch!, nil, 1);
-//                print("Branch not found")
-//                master = "\(branch!)"
-//            }
-//        }
-//
-//        if(master == ""){
-//            master = "refs/heads/master" // Prevents a crash below
-//        }
-//
-//
-//        print(master)
-//
-//        let strings: UnsafeMutablePointer<UnsafeMutablePointer<CChar>?> = [master].withUnsafeBufferPointer {
-//            let buffer = UnsafeMutablePointer<UnsafeMutablePointer<CChar>?>.allocate(capacity: $0.count + 1)
-//            let val = $0.map
-//            { $0.withCString(strdup) }
-//            buffer.initialize(from: val, count: 1)
-//            buffer[$0.count] = nil
-//            return buffer
-//        }
-//        var gitstr = git_strarray()
-//        gitstr.strings = strings
-//        gitstr.count = 1
-//
-//        let push_result = git_remote_push(remote, &gitstr, &options)
-//        print(push_result)
-//        git_remote_free(remote)
     }
     
     
