fafe8d4181cee4c2e659e92f0ca4bfd2642da13d
papavhub <pavi03@kpu.ac.kr>
2022-06-20 14:50:59 +0900

    Feat: Terraform + ingress

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -55,12 +55,129 @@ resource "aws_security_group" "instance" { // security group
       cidr_blocks = ["0.0.0.0/0"]
     }
 
+
+
+
     ingress {
       from_port = 1111
       to_port = 1111
       protocol = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
     }
+
+    ingress {
+      from_port = 8001
+      to_port = 8001
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+      from_port = 8081
+      to_port = 8081
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+      from_port = 8005
+      to_port = 8005
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+      from_port = 8085
+      to_port = 8085
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+      from_port = 8006
+      to_port = 8006
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+      from_port = 8086
+      to_port = 8086
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+      from_port = 8007
+      to_port = 8007
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+      from_port = 8087
+      to_port = 8087
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+      from_port = 9090
+      to_port = 9090
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+      from_port = 3000
+      to_port = 3000
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+      from_port = 9093
+      to_port = 9093
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+      from_port = 2181
+      to_port = 2181
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+      from_port = 9092
+      to_port = 9092
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+      from_port = 2222
+      to_port = 2222
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+      from_port = 3333
+      to_port = 3333
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+      from_port = 4444
+      to_port = 4444
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+
+
 
     egress {
       from_port = 0
